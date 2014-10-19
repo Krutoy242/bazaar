468140dc6ae9ce76d99356e575f185326f832890
Krutoy242 <Krutoy242@gmail.com>
2014-10-19 22:00:54 +0700

    Create servConnect.lua

---
+++ oc/bazaar/servConnect.lua
@@ -0,0 +1,21 @@
+-- Запрос таблицы предметов
+local function getLocalStorage()
+
+  return {}
+end
+
+-- Команда перемещения предметов между пользователями
+-- queryArray = { {bunchID, count}, [...] }
+-- bunchID - идентификатор кучки предметов в таблице
+-- count   - количество предметов из кучки
+-- player  - ник игрока, КОМУ переходят предметы
+local function transfer(queryArray, player)
+
+  return errorString
+end
+
+-- Команда выдачи из локального хранилища в сундук терминала
+local issue(queryArray)
+  
+  return errorString
+end
